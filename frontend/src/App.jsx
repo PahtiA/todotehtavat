33f1afcc3d6ef7609b5ee69148d4c079b562d699
t3paaa01 <t3paaa01@students.oamk.fi>
2025-09-01 18:50:51 +0300

    todo 4 valmis

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,22 +3,21 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Row from './components/Row'
 
-const url = "http://localhost:3002" // tarkista, että serverisi portti vastaa tätä
+const url = "http://localhost:3002"
 
 function App() {
   const [task, setTask] = useState('')
   const [tasks, setTasks] = useState([])
 
-  // Hae tehtävät kun komponentti mountataan
+
   useEffect(() => {
     axios.get(url)
       .then(response => setTasks(response.data))
       .catch(error => alert(error.response?.data?.message || error))
   }, [])
 
-  // Lisää uusi tehtävä
   const addTask = () => {
-    if (!task.trim()) return // estä tyhjän lisääminen
+    if (!task.trim()) return
     const newTask = { description: task }
     axios.post(url + "/create", { task: newTask })
       .then(response => {
@@ -28,7 +27,6 @@ function App() {
       .catch(error => alert(error.response?.data?.error || error))
   }
 
-  // Poista tehtävä
   const deleteTask = (id) => {
     axios.delete(`${url}/delete/${id}`)
       .then(() => setTasks(tasks.filter(t => t.id !== id)))
