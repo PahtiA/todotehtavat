84239c979a228354560efb6177113cdc7deaf9c8
t3paaa01 <t3paaa01@students.oamk.fi>
2025-09-07 18:15:18 +0300

    todo 7 valmis

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,18 @@ import './App.css'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Row from './components/Row'
+import { useUser } from './context/useUser'
 
-const url = "http://localhost:3002"
+const url = import.meta.env.VITE_API_URL
 
 function App() {
   const [task, setTask] = useState('')
   const [tasks, setTasks] = useState([])
-
+  const { user } = useUser()
 
   useEffect(() => {
-    axios.get(url)
+    const headers = { headers: { Authorization: user.token } }
+    axios.get(url, headers)
       .then(response => setTasks(response.data))
       .catch(error => alert(error.response?.data?.message || error))
   }, [])
@@ -19,7 +21,8 @@ function App() {
   const addTask = () => {
     if (!task.trim()) return
     const newTask = { description: task }
-    axios.post(url + "/create", { task: newTask })
+    const headers = { headers: { Authorization: user.token } }
+    axios.post(url + "/create", { task: newTask }, headers)
       .then(response => {
         setTasks([...tasks, response.data])
         setTask('')
@@ -28,7 +31,8 @@ function App() {
   }
 
   const deleteTask = (id) => {
-    axios.delete(`${url}/delete/${id}`)
+    const headers = { headers: { Authorization: user.token } }
+    axios.delete(`${url}/delete/${id}`, headers)
       .then(() => setTasks(tasks.filter(t => t.id !== id)))
       .catch(error => alert(error.response?.data?.error || error))
   }
